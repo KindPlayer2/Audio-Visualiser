3298d805c1c6537a2ca4f16391f90bf0d0fbc6fb
Grace <gracerosevalentine@gmail.com>
2023-04-05 23:54:51 +0100

    flower file

---
+++ java/src/ie/tudublin/Flower.java
@@ -0,0 +1,36 @@
+package ie.tudublin;
+
+import ddf.minim.Minim;
+import ddf.minim.AudioPlayer;
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.analysis.FFT;
+
+public class Flower {
+
+
+    Minim m;
+    AudioInput ai;
+    AudioPlayer ap;
+    AudioBuffer ab;
+
+    FFT fft;
+
+    public void settings()
+    {
+        size(1024, 1024);
+    }
+
+    public void setup()
+    {
+
+    }
+
+    float[] lerpedBuffer;
+    public void draw()
+    {
+
+    }
+
+    
+}
