0a0528b9a69865c99fe2755d203b7e93a79ab9a6
Grace <gracerosevalentine@gmail.com>
2023-04-21 21:31:55 +0100

    flower works

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -14,11 +14,17 @@ public class Main
         processing.core.PApplet.runSketch( a, new tree());
     }
 
-	
+
+	public static void Flower()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new flower());
+    }
+
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
-		Tree();
+		Flower();
 	}
 	
 	
